19c3dddc3f53c2c316aba7b6e9e91376d0d10fb7
EnzaEnzi <enza.webdev@gmail.com>
2019-04-22 21:59:35 +0200

    backup : barquette

--- functions/page/custom-settings/page-emporter.php
+++ functions/page/custom-settings/page-emporter.php
@@ -33,7 +33,7 @@ add_settings_section(
     'page-emporter'                                             // PAGE
 ); // end --> section : section_emporterpage_cover
 
-// FIELDS : emporterpage_cover_hidden ----------------------------------------------
+// FIELDS : emporterpage_cover_hidden ------------------------------------------
 add_settings_field(
     'emporterpage_cover_hidden',                                // ID
     __('Cacher la section', 'section_emporterpage_cover'),      // LABEL
@@ -42,7 +42,7 @@ add_settings_field(
     'section_emporterpage_cover'                                // SECTION ID
 ); // end --> field : emporterpage_cover_hidden
 
-// FIELDS : emporterpage_cover_bg_img ----------------------------------------------
+// FIELDS : emporterpage_cover_bg_img ------------------------------------------
 add_settings_field(
     'emporterpage_cover_bg_img',                                  // ID
     __('Image d\'arrière plan', 'section_emporterpage_cover'),    // LABEL
@@ -53,7 +53,7 @@ add_settings_field(
 
 
 
-// FIELDS : emporterpage_cover_titre -----------------------------------------------
+// FIELDS : emporterpage_cover_titre -------------------------------------------
 add_settings_field(
     'emporterpage_cover_titre',                                 // ID
     __('Titre de la section', 'section_emporterpage_cover'),    // LABEL
@@ -62,7 +62,7 @@ add_settings_field(
     'section_emporterpage_cover'                                // SECTION ID
 ); // end --> field : emporterpage_cover_titre
 
-// FIELDS : emporterpage_cover_texte -----------------------------------------------
+// FIELDS : emporterpage_cover_texte -------------------------------------------
 add_settings_field(
     'emporterpage_cover_texte',                                 // ID
     __('Texte', 'section_emporterpage_cover'),                  // LABEL
@@ -72,7 +72,7 @@ add_settings_field(
 ); // end --> field : emporterpage_cover_texte
 
 
-// REGISTER : section_emporterpage_cover -------------------------------------------
+// REGISTER : section_emporterpage_cover ---------------------------------------
 register_setting('group-page-emporter', 'emporterpage_cover_hidden');
 register_setting('group-page-emporter', 'emporterpage_cover_affiche_img');
 register_setting('group-page-emporter', 'emporterpage_cover_titre');
@@ -82,5 +82,191 @@ register_setting('group-page-emporter', 'emporterpage_cover_texte');
 register_setting(
     'group-page-emporter',                    // group (element display in the form)
     'emporterpage_cover_bg_img',              // field ID
-    'handle_file_bg_emporterpage'      // Callback function
+    'handle_file_bg_emporterpage'             // Callback function
 ); // end -> register : handle_file_bg_emporterpage
+
+/* ----------------------------------------------------------------------------- */
+// SECTION 2 : section_message --> Option 3 -- page-emporter
+/* ----------------------------------------------------------------------------- */
+// SETTINGS
+add_settings_section(
+    'section_emporterpage_message',                                   // ID
+    __('Section 2 -  Message', 'section_emporterpage_message'),       // TITLE
+    'display_section_emporterpage_message',                           // CALLBACK
+    'page-emporter'                                                   // PAGE
+); // end --> section : section_emporterpage_message
+
+// FIELDS : emporterpage_msg_hidden --------------------------------------------
+add_settings_field(
+    'emporterpage_msg_hidden',                                    // ID
+    __('Cacher la section', 'section_emporterpage_message'),      // LABEL
+    'field_emporterpage_msg_hidden',                              // CALLBACK FUNCTION
+    'page-emporter',                                              // MENU PAGE SLUG
+    'section_emporterpage_message'                                // SECTION ID
+); // end --> field : emporterpage_msg_hidden
+
+// FIELDS : emporterpage_msg_img_avatar ----------------------------------------
+add_settings_field(
+    'emporterpage_msg_img_avatar',                                // ID
+    __('Avatar', 'section_emporterpage_message'),                 // LABEL
+    'field_emporterpage_msg_img_avatar',                          // CALLBACK FUNCTION
+    'page-emporter',                                              // MENU PAGE SLUG
+    'section_emporterpage_message'                                // SECTION ID
+); // end -> field : emporterpage_msg_img_avatar
+
+// FIELDS : emporterpage_msg_txt -----------------------------------------------
+add_settings_field(
+    'emporterpage_msg_txt',                                     // ID
+    __('Texte accroche', 'section_emporterpage_message'),       // LABEL
+    'field_emporterpage_msg_txt',                               // CALLBACK FUNCTION
+    'page-emporter',                                            // MENU PAGE SLUG
+    'section_emporterpage_message'                              // SECTION ID
+); // end --> field : emporterpage_msg_txt
+
+
+// REGISTER : section_emporterpage_message -------------------------------------
+register_setting('group-page-emporter', 'emporterpage_msg_hidden');
+register_setting('group-page-emporter', 'emporterpage_msg_affiche_avatar');
+register_setting('group-page-emporter', 'emporterpage_msg_txt');
+
+// REGISTER : emporterpage_msg_img_avatar
+register_setting(
+    'group-page-emporter',                             // group (element display in the form)
+    'emporterpage_msg_img_avatar',                     // field ID
+    'handle_file_avatar_msg_emporterpage'              // Callback function
+); // end -> register : handle_file_avatar_msg_emporterpage
+
+
+/* ----------------------------------------------------------------------------- */
+// SECTION 3 : section_emporterpage_info --> Option 3 -- page-emporter
+/* ----------------------------------------------------------------------------- */
+
+// SETTINGS
+add_settings_section(
+    'section_emporterpage_info',                                           // ID
+    __('Section 3 - Carte à emporter', 'section_emporterpage_info'),       // TITLE
+    'display_section_emporterpage_info',                                   // CALLBACK
+    'page-emporter'                                                        // PAGE
+); // end --> section : section_emporterpage_info
+
+// FIELDS : emporterpage_info_avatar -------------------------------------------
+add_settings_field(
+    'emporterpage_info_avatar',                                         // ID
+    __('Avatar pour cette section', 'section_emporterpage_info'),       // LABEL
+    'field_emporterpage_info_avatar',                                   // CALLBACK FUNCTION
+    'page-emporter',                                                    // MENU PAGE SLUG
+    'section_emporterpage_info'                                         // SECTION ID
+); // end --> field : emporterpage_info_avatar
+
+// FIELDS : emporterpage_info_promo --------------------------------------------
+add_settings_field(
+    'emporterpage_info_promo',                                    // ID
+    __('Promo', 'section_emporterpage_info'),                     // LABEL
+    'field_emporterpage_info_promo',                              // CALLBACK FUNCTION
+    'page-emporter',                                              // MENU PAGE SLUG
+    'section_emporterpage_info'                                   // SECTION ID
+); // end --> field : emporterpage_info_promo
+
+
+// REGISTER : display_section_emporterpage_info -------------------------------------
+register_setting('group-page-emporter', 'emporterpage_promo_reduction');
+register_setting('group-page-emporter', 'emporterpage_promo_offert');
+register_setting('group-page-emporter', 'emporterpage_promo_minimum');
+
+// REGISTER : emporterpage_info_avatar
+register_setting(
+    'group-page-emporter',    // group (element display in the form)
+    'emporterpage_info_avatar', // field ID
+    'handle_file_avatar_info_emporterpage' // Callback function
+); // end -> register : handle_file_avatar_info_emporterpage
+
+
+/* ----------------------------------------------------------------------------- */
+// SECTION 4 : section-commander --> Option 3 -- page-emporter
+/* ----------------------------------------------------------------------------- */
+// SETTINGS
+add_settings_section(
+    'section_emporterpage_commander',                               // ID
+    __('Section 4 - Commander', 'section_emporterpage_commander'),  // TITLE
+    'display_section_emporterpage_commander',                       // CALLBACK
+    'page-emporter'                                                 // PAGE
+); // end --> section : section_emporterpage_cover
+
+// FIELDS : emporterpage_commander_hidden --------------------------------------
+add_settings_field(
+    'emporterpage_commander_hidden',                                // ID
+    __('Cacher la section', 'section_emporterpage_commander'),      // LABEL
+    'field_emporterpage_commander_hidden',                          // CALLBACK FUNCTION
+    'page-emporter',                                                // MENU PAGE SLUG
+    'section_emporterpage_commander'                                // SECTION ID
+); // end --> field : emporterpage_cover_hidden
+
+
+// REGISTER : section-commander
+register_setting('group-page-emporter', 'emporterpage_commander_hidden');
+
+/* ----------------------------------------------------------------------------- */
+// SECTION 5 : section-buffet --> Option 3 -- page-emporter
+/* ----------------------------------------------------------------------------- */
+
+// SETTINGS
+add_settings_section(
+    'section_emporterpage_buffet',                                       // ID
+    __('Section 5 - Buffet à emporter', 'section_emporterpage_buffet'),  // TITLE
+    'display_section_emporterpage_buffet',                               // CALLBACK
+    'page-emporter'                                                      // PAGE
+); // end --> section : section_emporterpage_cover
+
+// FIELDS : emporterpage_avatar_buffet -----------------------------------------
+add_settings_field(
+    'emporterpage_avatar_buffet',                                       // ID
+    __('Avatar pour cette section', 'section_emporterpage_buffet'),     // LABEL
+    'field_emporterpage_avatar_buffet',                                 // CALLBACK FUNCTION
+    'page-emporter',                                                    // MENU PAGE SLUG
+    'section_emporterpage_buffet'                                       // SECTION ID
+); // end --> field : emporterpage_avatar_buffet
+
+// FIELDS : emporterpage_barquette_buffet --------------------------------------
+add_settings_field(
+    'emporterpage_barquette_buffet',                                // ID
+    __('Systeme de barquette', 'section_emporterpage_buffet'),      // LABEL
+    'field_emporterpage_barquette_buffet',                          // CALLBACK FUNCTION
+    'page-emporter',                                                // MENU PAGE SLUG
+    'section_emporterpage_buffet'                                   // SECTION ID
+); // end --> field : emporterpage_barquette_buffet
+
+
+
+
+// REGISTER : section_emporter_buffet
+
+
+// REGISTER : emporter_buffet_avatar
+register_setting(
+    'group-page-emporter',    // group (element display in the form)
+    'emporter_buffet_avatar', // field ID
+    'handle_file_avatar_buffet_emporterpage' // Callback function
+); // end -> register : handle_file_avatar_buffet_emporterpage
+
+
+// REGISTER : emporterpage_formule_uno
+register_setting(
+    'group-page-emporter',                        // group (element display in the form)
+    'emporterpage_formule_uno',                  // field ID
+    'handle_file_formule_one_emporterpage'             // Callback function
+); // end -> register : emporterpage_formule_uno
+
+// REGISTER : emporterpage_formule_due
+register_setting(
+    'group-page-emporter',                        // group (element display in the form)
+    'emporterpage_formule_due',                  // field ID
+    'handle_file_formule_due_emporterpage'             // Callback function
+); // end -> register : emporterpage_formule_due
+
+
+// REGISTER : emporterpage_formule_tre
+register_setting(
+    'group-page-emporter',                        // group (element display in the form)
+    'emporterpage_formule_tre',                  // field ID
+    'handle_file_formule_tre_emporterpage'             // Callback function
+); // end -> register : emporterpage_formule_tre
--- page-emporters.php
+++ page-emporters.php
@@ -39,15 +39,174 @@
     }
 ?>
 
-
 <!-- START section 2 : section-emporterpage_message -->
+<?php
+    // SI emporterpage_cover_hidden EST COCHE
+    // => Alors il n'y a pas de section
+
+    if(checked(1, get_option('emporterpage_message_hidden'), false)){
+        ?>
+        <?php
+    } else {
+        // SINON
+        // => Afficher la section
+        ?>
+        <section id="message-carte" class="container section-message">
+            <div class="row">
+
+                <div class="col-md-6 col-12">
+                    <p class="acchoche"><?php echo get_option('emporterpage_msg_txt'); ?></p>
+                </div><!-- / .col-md-6 col-12 -->
+
+                <?php
+                // SI emporterpage_msg_affiche_avatar EST COCHE
+                // => Alors on affiche l'avatar
+
+                if(checked(1, get_option('emporterpage_msg_affiche_avatar'), false)){
+                    ?>
+                    <div class="col-md-6 col-12">
+                        <img src="<?php echo get_option('emporterpage_msg_img_avatar'); ?>" alt="" class="msg-avatar" />
+                    </div><!-- .col-md-6 .col-12 -->
+                    <?php
+                }
+                ?>
+
+            </div><!-- / .row -->
+        </section><!-- / #menu-section-carte .bg-carte-->
+        <?php
+    }
+?>
+
+<!-- START section 3 : carte-emporter -->
+<section id="carte-emporter" class="container">
+    <h1>Carte à emporter</h1>
+    <div class="row">
+        <div class="col-3">
+            <img src="<?php echo get_option('emporterpage_info_avatar'); ?>" alt="">
+        </div><!-- /.col-3 -->
+
+        <div class="col-9">
+            <ul>
+                <li>
+                    <strong><?php echo get_option('emporterpage_promo_reduction'); ?></strong>
+                    sur tout les plats à emporter
+                </li>
+                <li>
+                    <strong>
+                        <?php echo get_option('emporterpage_promo_offert'); ?> <span> offert</span>
+                    </strong>
+                    <span>a partir de </span>
+                    <span><?php echo get_option('emporterpage_promo_minimum'); ?></span>
+                </li>
+            </ul>
+        </div><!-- /.col-9 -->
+    </div><!-- /.row -->
+
+    <!-- region repete => CPT-Emporter -->
+    <div class="">
+        Ici région répeter Filter
+    </div>
+
+    <div class="row">
+        <!-- region repete => CPT-Emporter -->
+        <div class="col-md-3 .col-12">
+            <h1>Titre plat</h1>
+            <div class="">
+                image
+            </div>
+        </div>
+        <div class="col-md-9 .col-12">
+            <!-- region repeter => metabox repeatable-field -->
+            Table
+        </div>
+    </div>
+
+</section><!-- / #carte-emporter .container -->
 
 
-<!-- START section 4 : carte-content -->
+<!-- START section 5 : section-commander -->
+<?php
+    // SI emporterpage_commander_hidden EST COCHE
+    // => Alors il n'y a pas de section
 
+    if(checked(1, get_option('emporterpage_commander_hidden'), false)){
+        ?>
+        <section class="commander text-center">
+            <h1>Passer commande</h1>
+            <p>
+                <?php echo get_option('inforesto_phone'); ?>
+            </p>
 
+        </section><!-- /  .bg-carte-->
+        <?php
+    }
+?>
 
-<!-- START section 5 : section-reservation -->
+<!-- START section 5 : buffet-emporter -->
+<section id="buffet-emporter" class="container">
+    <div class="text-center">
+        <h1>Buffet à emporter</h1>
+        <p>faites votre choix dans les entrées et plats préparés</p>
+    </div>
 
+    <div class="row">
+        <div class="col-md-7 col-12 box-left">
+            <ul>
+
+                    <!-- item-buffet-takeaway -->
+                <li class="item-buffet-takeaway row">
+                    <div class="col-4">
+                        <img src="" alt="">
+                    </div>
+
+                    <div class="col-5">
+                        <p>1 barquette</p>
+                    </div>
+
+                    <div class="col-3">
+                        <p class="prix">8,00€</p>
+                    </div>
+
+                </li><!-- /.item-buffet-takeaway -->
+
+                <!-- item-buffet-takeaway -->
+                <li class="item-buffet-takeaway row">
+                    <div class="col-4">
+                        <img src="" alt="">
+                    </div>
+
+                    <div class="col-5">
+                        <p>1 barquette</p>
+                    </div>
+
+                    <div class="col-3">
+                        <p class="prix">8,00€</p>
+                    </div>
+
+                </li><!-- /.item-buffet-takeaway -->
+
+                <!-- item-buffet-takeaway -->
+                <li class="item-buffet-takeaway row">
+                    <div class="col-4">
+                        <img src="" alt="">
+                    </div>
+
+                    <div class="col-5">
+                        <p>1 barquette</p>
+                    </div>
+
+                    <div class="col-3">
+                        <p class="prix">8,00€</p>
+                    </div>
+
+                </li><!-- /.item-buffet-takeaway -->
+            </ul>
+        </div><!-- /.box-left -->
+        <div class="col-md-5 box-right">
+            <img src="<?php echo get_option('emporter_buffet_avatar'); ?>" alt="">
+        </div><!-- /.box-right -->
+    </div><!-- /.row -->
+
+</section><!-- / #buffet-emporter .container -->
 
 <?php get_footer(); ?>
